fb3cb83706947205f6beb6e3d3f750788249edaf
Ankush Gangrade <ankush.gangrade@worldpay.com>
2016-07-22 13:19:46 +0530

    name

--- src/com/test/First.java
+++ src/com/test/First.java
@@ -13,21 +13,21 @@ public class First {
 	/**
 	 * @param name the name to set
 	 */
-	public void setName(String name) {
+	/*public void setName(String name) {
 		this.name = name;
-	}
+	}*/
 	
 	/**
 	 * @return the rollNo
 	 */
-	/*public String getRollNo() {
+	public String getRollNo() {
 		return rollNo;
-	}*/
+	}
 
 	/**
 	 * @param name the rollNo to set
 	 */
-	/*public void setRollNo(String rollNo) {
+	public void setRollNo(String rollNo) {
 		this.rollNo = rollNo;
-	}*/
+	}
 }
